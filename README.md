31f7f1943ce9069d39e465a7fe041b73d1e39b69
Reid Schroder <reidschroder2@gmail.com>
2022-03-17 15:17:33 -0400

    finished challenge

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# E-commerce Back End Starter Code
+# E-commerce 
+
+## Purpose
+A Node.js application that uses sequelize and express to pull and create information in a database for the back end of an e-commerce website.
+
+This project uses object relational mapping to modularize get, post, put, and delete routes in order to create the cleanest code we can to keep a highly functional application organized.
+
+## Built With
+* JavaScript
+* Sequelize
+* Express
+* SQL
+
+## Demonstration Video
+[Demo](https://drive.google.com/file/d/1szctI7kF7ScoDsM_Fty7G555C4aJ2pqR/view)
+
+## Examples
+![Alt text](./images/get-all-categories.png "Get All Categories")
+
+![Alt text](./images/get-category-by-id.png "Get Category By id")
+
+![Alt text](./images/post-product.png "Post Product")
+
+
+## Contribution
+Reid Schroder
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -13,7 +13,7 @@ router.get('/', (req, res) => {
       {model: Tag}
     ]
   })
-  .then(dbPostData => res.json(dbPostData))
+  .then(dbProductData => res.json(dbProductData))
   .catch(err => {
     console.log(err);
     res.status(500).json(err);
@@ -29,7 +29,7 @@ router.get('/:id', (req, res) => {
     },
     include: [Category, Tag]
   })
-  .then(dbPostData => res.json(dbPostData))
+  .then(dbProductData => res.json(dbProductData))
   .catch(err => {
     console.log(err);
     res.status(500).json(err);
@@ -113,13 +113,13 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
-  Post.destroy({
+  Product.destroy({
     where: {
       id: req.params.id
     }
   })
-  .then(dbPostData => {
-    if (!dbPostData) {
+  .then(dbProductData => {
+    if (!dbProductData) {
       res.status(404).json({ message: 'No Products found with this id'})
     }
   })
